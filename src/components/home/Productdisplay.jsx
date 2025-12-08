9f1acc2583f94800d4a6e708672927a829bb8c08
vickyvera-dev <amvickyvera1@gmail.com>
2025-12-08 02:12:16 +0100

    slide responsiveness fixed

--- src/components/home/Productdisplay.jsx
+++ src/components/home/Productdisplay.jsx
@@ -54,23 +54,32 @@ function Productdisplay() {
  const settings = {
   dots: true,
   infinite: true,
-  slidesToShow: 5,
+  slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2500,
   pauseOnHover: true,
-  // centerMode: true,
+  centerMode: true,
   arrows: true,
   nextArrow: <ArrowRight />,
   prevArrow: <ArrowLeft />,
   responsive: [
+       {
+      breakpoint: 1440, // <=1440px
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+         centerMode: false
+      }
+    },
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
+          dots: true,
+           centerMode: false
         }
       },
       {
@@ -78,14 +87,16 @@ function Productdisplay() {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
+          initialSlide: 2,
+           centerMode: false
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+           centerMode: false
         }
       }
     ]
