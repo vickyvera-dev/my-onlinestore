4f75f849c2c4b45805a9bd40a16e5f98cd327785
vickyvera-dev <amvickyvera1@gmail.com>
2025-12-08 12:42:22 +0100

    changes

--- src/components/home/Productdisplay.jsx
+++ src/components/home/Productdisplay.jsx
@@ -104,11 +104,11 @@ function Productdisplay() {
       </div>
 
       {/* Product Carousel */}
-      <div className="lg:max-w-7xl w-full overflow-hidden mx-auto px-4 md:px-8 relative">
+      <div className="lg:max-w-7xl w-full overflow-x-auto mx-auto px-4 md:px-8 relative">
         <Slider {...settings}>
           {items.map((item) => (
             <div key={item.id}>
-              <div  className="px-1 lg:px-2 sm:px-1 md:px-3 w-full flex justify-center">
+              <div  className="px-1 lg:px-2 sm:px-1 md:px-3 w-full flex justify-center items-center">
                 <div
                 data-aos="zoom-in"
                 className="bg-gray-100 rounded-2xl p-6 text-center shadow-sm hover:shadow-md transition-all duration-500 ease-in-out hover:-translate-y-2 w-full"
